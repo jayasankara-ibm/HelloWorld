1cc6626deb128715dfe3794dbe2768da2894f88b
Jayasankara Reddy <jayasankara@in.ibm.com>
2018-08-17 00:07:43 +0530

    test

---
+++ src/HelloWorldTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+//		new HelloWorld().testMessage();
+	}
+
+	@Test
+	public void testTestMessage() {
+		new HelloWorld().testMessage();
+	}
+}
